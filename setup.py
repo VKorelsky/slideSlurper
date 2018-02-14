9ac611f77d8846c9ffa7c0adf6d113ec28b5c07f
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2018-02-14 13:40:52 -0500

    and another one

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='slideSlurper',
-    version='1.0.5',
+    version='1.0.6',
     description='slurp slides from lecture videos',
     url='https://github.com/vkorelsky/slideSlurper',
     author='Victor Korelsky',
