0a0e5b64032dae5cf82cfa482730499e38bf11b2
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2018-02-14 12:41:21 -0500

    changes to setup

--- setup.py
+++ setup.py
@@ -4,35 +4,16 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='slideSlurper',
     version='1.0.0',
     description='slurp slides from lecture videos',
-    long_description=long_description,
     url='https://github.com/vkorelsky/slideSlurper',
     author='Victor Korelsky',
     author_email='victor.korelsky@mail.mcgill.ca',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-
-        'Intended Audience :: Other audience',
-        'Topic :: Multimedia :: Video :: Conversion',
-
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7'
-
-        # untested with versions superior to python 2.7
-    ],
-
+    license='MIT',
     keywords='video capture lectures',
-
     packages=find_packages('.'),
-
     install_requires=[
         'tqdm',
         'cv2',
@@ -40,7 +21,6 @@ setup(
         'fdpf',
         'PIL'
         ],
-
     entry_points={
         'console_scripts': [
             'slurp=slideSlurper:main',
